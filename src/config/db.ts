9245786a5e4983548ea2f8be7873d90b04eb3c53
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-04-09 00:52:20 +0300

    Connect to the db and fetch data from there

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+
+export default function createPool() {
+    return new Pool({
+	host: 'localhost',
+	user: 'postgres',
+	password: 'postgres',
+	database: 'central',
+	port: 5432,
+    });
+}
