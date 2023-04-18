aea15ddae8e210aea8dc00718cd585a8e4921bbc
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-04-18 22:36:33 +0300

    Add user controller with POST request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 
 import relayRoutes from './routes/relayRoutes';
+import userRoutes from './routes/userRoutes';
 
 const app: Application = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ const port: number = 3000;
 const version: string = '/api/v1'
 
 app.use(version, relayRoutes);
+app.use(version, userRoutes);
 
 app.listen(port, () => {
     console.log(`Connected successfully on port ${port}`);
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import {
+    postUser,
+} from '../controllers/userController'
+
+const router = express.Router();
+
+router.post('/users', postUser);
+
+export default router;
