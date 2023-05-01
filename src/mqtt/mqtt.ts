2a94f610ed1699164c7a79598c0a2949276187c5
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-05-01 13:57:28 +0300

    Add sendMqttMessage function

---
+++ src/mqtt/mqtt.ts
@@ -0,0 +1,32 @@
+import mqtt, { MqttClient } from 'mqtt';
+
+const mqttBrokerAddress = 'mqtt://localhost'; // TODO: Use environment variable
+
+function sendMqttMessage(mqttTopic: string, mqttMessage: string,
+			 mqttUsername: string, mqttPassword: string): void {
+    const client: MqttClient = mqtt.connect(mqttBrokerAddress, {
+	username: mqttUsername,
+	password: mqttPassword
+    });
+
+    client.on('connect', function () {
+	console.log('connected to MQTT broker');
+	client.subscribe(mqttTopic, function (err: Error) {
+	    if (err) {
+		console.error(`failed to subscribe to MQTT topic: ${mqttTopic}`);
+	    } else {
+		console.log(`subscribed to MQTT topic: ${mqttTopic}`);
+		client.publish(mqttTopic, mqttMessage);
+		console.log(`published message: '${mqttMessage}' to MQTT topic: ${mqttTopic}`);
+	    }
+	});
+    });
+
+    client.on('error', function (err: Error) {
+	console.error('failed to connect to MQTT broker', err);
+    });
+}
+
+export {
+    sendMqttMessage,
+}
