f1dfa23c791bc684c20f23fbcf799a321b277bdc
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-05-06 02:34:08 +0300

    Lint fix

--- src/controllers/relayControllers.ts
+++ src/controllers/relayControllers.ts
@@ -8,14 +8,14 @@ interface RoutineError {
 
 const pool = createPool();
 
-const getAllRelays = async(req: Request, res: Response ) => {
-    try {
-	const result = await pool.query('SELECT * FROM relays');
-	const metadata = { count: result.rows.length };
-	res.status(200).json({ data: result.rows, metadata: metadata });
-    } catch (error) {
-	res.status(500).json({ message: 'Server error' });
-    }
+const getAllRelays = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query('SELECT * FROM relays');
+    const metadata = { count: result.rows.length };
+    res.status(200).json({ data: result.rows, metadata: metadata });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 const getRelayById = async (req: Request, res: Response) => {
