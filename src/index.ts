54bead812eb48c647cfac595f90859d0e6592c43
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-05-06 20:37:20 +0300

    Use environemnt variables in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import deviceTypes from './routes/deviceTypesRoutes';
 
 const app: Application = express();
 app.use(express.json());
-const port = 3000;
+
+const hostname = process.env.API_HOSTNAME as string;
+const port = Number(process.env.API_PORT);
 
 const version = '/api/v1';
 
@@ -16,6 +18,6 @@ app.use(version, userRoutes);
 app.use(version, roomRoutes);
 app.use(version, deviceTypes);
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Connected successfully on port ${port}`);
 });
