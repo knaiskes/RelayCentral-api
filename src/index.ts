0fe18f7e28f3e2b109c22606726bbbc97bb4d454
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-06-05 02:32:31 +0300

    Add jwt token authorization middleware and use it for the relays route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 import relayRoutes from './routes/relayRoutes';
 import userRoutes from './routes/userRoutes';
 import roomRoutes from './routes/roomRoutes';
 import deviceTypes from './routes/deviceTypesRoutes';
+import loginRoutes from './routes/loginRoutes';
+
+import { verifyToken } from './middleware/authMiddleware';
 
 const app: Application = express();
 app.use(express.json());
@@ -13,10 +16,11 @@ const port = Number(process.env.API_PORT);
 
 const version = '/api/v1';
 
-app.use(version, relayRoutes);
+app.use(version, verifyToken, relayRoutes);
 app.use(version, userRoutes);
 app.use(version, roomRoutes);
 app.use(version, deviceTypes);
+app.use(version, loginRoutes);
 
 app.listen(port, hostname, () => {
     console.log(`Connected successfully on port ${hostname} : ${port}`);
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const secretKey = process.env.TOKEN_SECRET as string;
+
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    const { path } = req;
+    const token = req.headers['authorization'];
+
+    // Skip token verification for the login route
+    if (path === '/login') {
+	next();
+	return;
+    }
+
+    if (!token) {
+	return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+	const decoded = jwt.verify(token as string, secretKey);
+	req.user = decoded as User;
+	next();
+    } catch (error) {
+	return res.status(403).json({ message: 'Failed to authenticate token' });
+    }
+};
