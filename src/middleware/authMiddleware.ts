16798e0a20ed2426fc86864b75025e73c06f7668
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-06-05 04:01:30 +0300

    Restore User type

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -21,7 +21,7 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
 
   try {
     const decoded = jwt.verify(token as string, secretKey);
-    req.user = decoded as any;
+    req.user = decoded as User;
     next();
   } catch (error) {
     return res.status(403).json({ message: 'Failed to authenticate token' });
